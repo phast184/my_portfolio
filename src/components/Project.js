f0ecf4de25ac16f5b38850a80a65cea6bc40ce68
phatnguyen1841999 <phatnguyen1841999@gmail.com>
2021-01-26 11:07:23 +0700

    update covid-19 tracker project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import './Project.css'
-const Project = ({project}) => {
-    return<a className = 'post' href = {project.url}>
-        <img src = {project.img} className = 'thumbnail' alt = {project.name} />
-        <div className = 'post-preview'>
-            <h6 className = 'post-title'>{project.name}</h6>
-            <p className = 'post-intro'>{project.description}</p>
-            <a href = {project.url}>Check it out</a>
+import React from "react";
+import "./Project.css";
+const Project = ({ project }) => {
+  return (
+    <article className="post">
+      <img src={project.img} className="thumbnail" alt={project.name} />
+      <div className="post-preview">
+        <h6 className="post-title">{project.name}</h6>
+        <p className="post-intro">{project.description}</p>
+        <p>{project.features}</p>
+        <div className="post-link">
+          <a href={project.demo}>Live demo</a>
+          <a href={project.github}>Source Code</a>
         </div>
-    </a>
-}
+      </div>
+    </article>
+  );
+};
 export default Project;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,41 +1,57 @@
 import React from "react";
 import Project from "./Project";
-import Pic1 from '../images/project1.jpg';
-import Pic2 from '../images/project2.jpg';
-import Pic3 from '../images/project3.jpg';
-import './ProjectList.css'
+import Pic1 from "../images/project1.jpg";
+import Pic2 from "../images/project2.jpg";
+import Pic3 from "../images/project3.jpg";
+import Pic4 from "../images/project4.JPG";
+import "./ProjectList.css";
 const ProjectList = () => {
   const projectList = [
     {
       img: Pic1,
-      name: 'Comfy Sloth Store',
+      name: "Comfy Sloth Store",
       description:
         "An e-commerce web app that allows customer to purchase suitable comfy.",
-      url: "https://comfy-sloth-store.netlify.app/",
+      features: "Features: display products, Cart functions, Login Authentication",
+      demo: "https://comfy-sloth-store.netlify.app/",
+      github: "https://github.com/phast184/compfy-sloth-store"
+    },
+    {
+      img: Pic4,
+      name: "Covid-19 Tracker",
+      description: "Displaying the statistics of worldwide data on COVID-19. ",
+      features:
+        "Features: Map, Line Graph, Case type of data. Chart of worldwide top countries",
+      demo: "https://covid-tracker-percy.netlify.app",
+      github:"https://github.com/phast184/covid_tracker"
     },
     {
       img: Pic2,
-      name: 'Movie Search',
+      name: "Movie Search",
       description:
         "A website I created that used OMDb API to search for plenty of movies in the database.",
-      url: "https://movie-dbsearch.netlify.app/",
+        features: "Features: Search movies by keywords, Movie details",
+      demo: "https://movie-dbsearch.netlify.app/",
+      github:"https://github.com/phast184/movie_search"
     },
     {
       img: Pic3,
-      name: 'Stock Photo',
+      name: "Stock Photo",
       description:
         "Display free stock photos from Unsplash. User can also use this app to search by keywords.",
-      url: "https://stock-photos-hehe.netlify.app/",
+      features: "Features: gallery of free stock photos, search photos by keywords",
+      demo: "https://stock-photos-hehe.netlify.app/",
+      github:"https://github.com/phast184/stock-photos"
     },
   ];
   return (
     <>
       <section className="s1">
         <div className="main-container">
-          <h3 id = 'project-title'>My projects</h3>
+          <h3 id="project-title">My projects</h3>
           <div className="post-wrapper">
             {projectList.map((project, index) => {
-              return <Project key={index} project = {project}/>;
+              return <Project key={index} project={project} />;
             })}
           </div>
         </div>
